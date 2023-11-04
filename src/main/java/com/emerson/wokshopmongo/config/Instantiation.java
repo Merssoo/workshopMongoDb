761c930ec33de916ab9c6baea365de15dd605b68
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-11-04 16:27:41 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/emerson/wokshopmongo/config/Instantiation.java
+++ src/main/java/com/emerson/wokshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.emerson.wokshopmongo.config;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.emerson.wokshopmongo.DTO.AuthorDTO;
+import com.emerson.wokshopmongo.DTO.CommentDTO;
 import com.emerson.wokshopmongo.domain.Post;
 import com.emerson.wokshopmongo.domain.User;
 import com.emerson.wokshopmongo.repository.PostRepository;
@@ -40,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
---
+++ src/main/java/com/emerson/wokshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.emerson.wokshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emerson.wokshopmongo.domain.Post;
+import com.emerson.wokshopmongo.repository.PostRepository;
+import com.emerson.wokshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
