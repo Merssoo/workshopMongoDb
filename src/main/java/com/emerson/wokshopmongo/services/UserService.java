8a5d705d627b2f4360a5f9c0717588b912caf8fc
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-11-04 14:40:07 -0300

    Inserção de usuário com POST

--- src/main/java/com/emerson/wokshopmongo/services/UserService.java
+++ src/main/java/com/emerson/wokshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emerson.wokshopmongo.DTO.UserDTO;
 import com.emerson.wokshopmongo.domain.User;
 import com.emerson.wokshopmongo.repository.UserRepository;
 import com.emerson.wokshopmongo.services.exception.ObjectNotFoundException;
@@ -25,5 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
