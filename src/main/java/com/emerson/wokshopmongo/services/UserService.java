101fa7b3b2391b00f30bfb7564db927429ddd162
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-11-04 14:13:36 -0300

    Obtendo um usuário por id

--- src/main/java/com/emerson/wokshopmongo/services/UserService.java
+++ src/main/java/com/emerson/wokshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.emerson.wokshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emerson.wokshopmongo.domain.User;
 import com.emerson.wokshopmongo.repository.UserRepository;
+import com.emerson.wokshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,10 @@ public class UserService {
 		return repo.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 }
